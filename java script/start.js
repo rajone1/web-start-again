66f413f53e8fe0a2eb5504cac6e516baf4f85fd1
rajone1 <rajoneweab101@gmail.com>
2023-07-29 00:08:35 +0600

    new start all

---
+++ java script/start.js
@@ -0,0 +1,21 @@
+console.log("start with me");
+
+console.log("wrong");
+console.log("wrong");
+var a = 10;
+var b = 5;
+console.log("a+b");
+
+var a = 10;
+var b = 10;
+var x = a + b;
+console.log(x);
+var a = 10;
+var b = 10;
+var x = a != b;
+console.log(x);
+20 > 10;
+var a = "10";
+var b = 10;
+var z = a !== b;
+console.log(z);
